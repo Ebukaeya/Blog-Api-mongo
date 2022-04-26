b6bf6a6381bb451450bbd500b47650340675cb45
Ebuka <eyaebuka@gmail.com>
2022-04-26 15:28:40 +0200

    server template.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
 import express from "express";
-import listEndpoints from "express/list/endpoints"
-import cors from "cors"
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
 import mongoose from "mongoose";
 
-const app = express()
+const app = express();
+let port = process.env.port
+const dbURl = process.env.url
 
+/* ---------------------------------------- middle wares--------------------------------------- */
 
+/* -----------------------------------------endpoints-------------------------------------------- */
 
+/* ----------------------------------------error handlers------------------------------------------ */
 
+/* connect db  */
 
+mongoose.connect(
+  dbURl
+);
 
-app.listen("3000",()=>{
+mongoose.connection.on("connected", () => {
+  app.listen(port, () => {
     console.log("listening on 3000");
-})
+    console.table(listEndpoints(app))
+  });
+});
