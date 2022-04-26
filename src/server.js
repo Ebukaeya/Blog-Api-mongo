d21099b7bdc9b7e9e3917dad3341f67a7808e5b0
Ebuka <eyaebuka@gmail.com>
2022-04-26 14:54:24 +0200

    .

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import listEndpoints from "express/list/endpoints"
+import cors from "cors"
+import mongoose from "mongoose";
+
+const app = express()
+
+
+
+
+
+
+app.listen("3000",()=>{
+    console.log("listening on 3000");
+})
